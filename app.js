6fdb73a41363c41e2e366f22b28870976f5d486d
Alessandro <alessandrofrancescocastelli@outlook.it>
2024-09-17 15:24:45 +0200

    Added ECMA script

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import mongoose from "mongoose";
+import {
+    signUp,
+    signUpPost,
+    login,
+    loginPost,
+} from "./controller/controller.js";
+//in package.json aggiungendo la proprietà "type" : "module" permettiamo l'utilizzo dei moduli ECMA
+//senza dover specificare l'estenzione diretta del file con .mjs
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json()); //per poter leggere file json (express non è in grado), trasfroma i file json in obj
+
+// view engine, permette di settare la vista del motore utilizzato, in questo caso ejs una forma di html e js
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI = "mongodb+srv://Alex:admin@cluster0.vdw2h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        app.listen(3000)
+        console.log("Server start at http://localhost:3000")
+    } catch (error) {
+        console.log(error);
+    }
+};
+connectDB()
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then((result) => app.listen(3000),console.log("Server start at http://localhost:3000"))
+//     .catch((err) => console.log(err));
+
+// routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies")); //indica i file in View
+
+//istanziamo le route, avremo le funzioni in controller.mjs
+//che si occuperanno di manipolare/controllare il nostro db
+app.get("/signup", signUp);
+app.post("/signup", signUpPost);
+app.get("/login", login);
+app.post("/login", loginPost);
