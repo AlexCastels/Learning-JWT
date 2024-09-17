17e6ff1207645eda73b06f5e1c4ef67b1e4304de
Alessandro <alessandrofrancescocastelli@outlook.it>
2024-09-17 17:14:20 +0200

    Added error handler in controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -1,5 +1,34 @@
 import User from "../models/User.js";
 
+//handle errors
+//così possiamo gestire meglio gli errori che recuperiamo dallo schema, avendo informazioni più dettagliate sul tipo di errore
+//l'errore globale altro non è che un obj con diverse proprietà che contengono informazioni su vari errori incontrati
+//anche gli errori che abbiamo specificato nello user schema, dunque per poter accedere a quei valori in maniera rapida
+//e più pulita, per far questo creiamo un obj vuoto e lo popoliamo dinamicamente tramite [], prima con Object.values recuperiamo
+//i valori di ogni key al suo interno, poi per ogni sua proprietà che destrutturiamo nel forEach matchiamo la key tramite .path
+//se corrisponde al nome esatto della proprietà specificata nell'obj assegnerà il nuovo valore .message
+function handleError(err){
+    console.log(err.message , err.code);
+    let errors = {email : '',password: ''}
+
+    //duplicate error code, l'errore di duplicazione sarà sempre 11000, se è presente questo codice modifica direttamente
+    //il val di email nell'obj vuoto
+    if(err.code === 11000){
+        errors.email = 'Email is already registered'
+        return errors
+    }
+
+    //validation errors
+    if(err.message.includes('user validation failed')){
+        //const properties = error.properties, direttamente viene destrutturato nel forEach
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message
+        })
+    }
+
+    return errors
+}
+
 export async function signUp(req , res){
     res.render('signup')
 }
@@ -14,8 +43,8 @@ export async function signUpPost(req , res){
         const user = await User.create({email ,password})
         res.status(201).json(user).send('new singup')
     } catch (error) {
-        console.log(error);
-        res.status(400).send("error, user not created")
+        const errors = handleError(error)
+        res.status(400).json(errors)
     }
 }
 
--- models/User.js
+++ models/User.js
@@ -1,16 +1,27 @@
 import mongoose from 'mongoose'
+import pkg from 'validator';
+const { isEmail } = pkg;
+
+//si importano tutti i pakage di validator e si destrutturano le funzioni che servono
+
+//così è come poter definire degli schermi tramite mongoose, per poter creare modelli per poter inserire dati
+//con le [] in required possiamo specificare prima il tipo, poi un msg per gestire gli errori
+//validate è un parametro che permette l'aggiunzione di una funzione personalizzata e non per poter creare un controllo
+//in questo caso viene utilizzata una lib esterna che presenta già una funzione per la verifica della mail. 
+//(scrittura personalizzata):validate: (val) => {} il val corrisponde al valore inserito nel'oggetto email 
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required : true ,
+        required : [true , 'Please enter an email'],
         unique: true ,
         lowercase : true,
+        validate: [isEmail, 'Please enter an valid email']
     },
     password:{
         type: String,
-        required : true ,
-        minLength: 6 ,
+        required : [true , 'Please enter an password'],
+        minlength: [6 , 'Minimum password length is 6 characters'],
     },
 })
 
